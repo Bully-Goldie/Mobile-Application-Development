3da9461f7399b121ac59b8c66ace232cc119b1f4
ivan <hcfygxxg@gmail.com>
2026-05-05 18:21:24 +0300

    pr12

---
+++ app/src/main/java/Components_pr11/ViewModel.kt
@@ -0,0 +1,13 @@
+package Components_pr11
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ViewModel : ViewModel() {
+    private val _categories = MutableStateFlow(categoryGet())
+    val categories = _categories.asStateFlow()
+
+    private val _analysisItems = MutableStateFlow(analysisGet())
+    val analysisItems = _analysisItems.asStateFlow()
+}
--- app/src/main/java/Screen_pr11/LazyColumnCards.kt
+++ app/src/main/java/Screen_pr11/LazyColumnCards.kt
@@ -1,42 +1,24 @@
 package Screen_pr11
 
-import Components_pr11.AnalysisItem
-import Components_pr11.Cards
-import Components_pr11.Categories
-import Components_pr11.analysisGet
-import Components_pr11.categoryGet
+import Components_pr11.ViewModel
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.shadow.Shadow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpOffset
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun LazyColumnCards(
     modifier: Modifier = Modifier,
-    itemsListCategory: List<String>,
-    analysisList: List<AnalysisItem>
+    viewModel: ViewModel = viewModel()
 ) {
-    val itemsListCategory = itemsListCategory
-    val analysisList = analysisList
+    val itemsListCategory by viewModel.categories.collectAsState()
+    val analysisList by viewModel.analysisItems.collectAsState()
+
 
     Column(
         modifier = Modifier.fillMaxSize().background(Color.White)
@@ -45,13 +27,4 @@ fun LazyColumnCards(
 
         ListCards(analysisList = analysisList)
     }
-}
-
-@Preview
-@Composable
-private fun LazyColumnCardsPrev() {
-    LazyColumnCards(
-        itemsListCategory = categoryGet(),
-        analysisList = analysisGet()
-    )
 }
